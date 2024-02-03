2582ea6c560dda91746c553fc1b06e59f70624f2
Mirko Mollik <mirko.mollik@fit.fraunhofer.de>
2024-02-03 14:45:04 +0100

    outcomment migration job
    Signed-off-by: Mirko Mollik <mirko.mollik@fit.fraunhofer.de>

--- viewer/scripts/merge-wallets.mjs
+++ viewer/scripts/merge-wallets.mjs
@@ -1,21 +1,21 @@
 import { readdirSync, readFileSync, writeFileSync } from 'fs';
 
 // function to parse the flattened_wallets.json and create individual wallet files
-const w = JSON.parse(readFileSync('../flattened_wallets.json'));
-w.forEach(element => {
-  element["$schema"] = "../schema.json";
+// const w = JSON.parse(readFileSync('../flattened_wallets.json'));
+// w.forEach(element => {
+//   element["$schema"] = "../schema.json";
 
-  const keys = Object.keys(element);
-  keys.forEach(key => {
-    if(element[key] === "") {
-      delete element[key];
-    }
-  });
+//   const keys = Object.keys(element);
+//   keys.forEach(key => {
+//     if(element[key] === "") {
+//       delete element[key];
+//     }
+//   });
 
-  //patch openSourceField to boolean
-  element.openSource = element.openSource === "Yes";
-  writeFileSync(`../wallets/${element.name}.json`, JSON.stringify(element, null, 2));
-});
+//   //patch openSourceField to boolean
+//   element.openSource = element.openSource === "Yes";
+//   writeFileSync(`../wallets/${element.name}.json`, JSON.stringify(element, null, 2));
+// });
 
 // function to merge all the individual wallet files into a single file
 const files = readdirSync('../wallets');
