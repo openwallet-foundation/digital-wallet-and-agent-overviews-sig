99ea503f639d17f64d5d3a0046d1861f7a9125e9
Samuel Rinnetmäki <samuel@iki.fi>
2024-04-12 10:21:09 +0300

    report and skip erroneous files
    Signed-off-by: Samuel Rinnetmäki <samuel@iki.fi>

--- viewer/scripts/merge-wallets.mjs
+++ viewer/scripts/merge-wallets.mjs
@@ -2,5 +2,14 @@ import { readdirSync, readFileSync, writeFileSync } from 'fs';
 
 // function to merge all the individual wallet files into a single file
 const files = readdirSync('../wallets');
-const wallets = files.map(file => JSON.parse(readFileSync(`../wallets/${file}`)));
+// const wallets = files.map(file => JSON.parse(readFileSync(`../wallets/${file}`)));
+const wallets = []
+for (const file of files) {
+  try {
+    wallets.push(JSON.parse(readFileSync(`../wallets/${file}`)))
+  }
+  catch(e) {
+    console.warn(`Error parsing ${file}: ${e}`)
+  }
+}
 writeFileSync('src/assets/wallets.json', JSON.stringify(wallets, null, 2));
