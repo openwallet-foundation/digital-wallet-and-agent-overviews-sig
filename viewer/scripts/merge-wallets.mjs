79fdc8563e26f7c53286e8a44dae012ff102e3ea
Samuel Rinnetmäki <samuel@iki.fi>
2024-04-16 03:47:56 -0700

    added status history
    Signed-off-by: Samuel Rinnetmäki <samuel@iki.fi>

--- viewer/scripts/merge-wallets.mjs
+++ viewer/scripts/merge-wallets.mjs
@@ -1,12 +1,17 @@
 import { readdirSync, readFileSync, writeFileSync } from 'fs';
 
+// url for the folder containing the wallet files in the GitHub repo
+const commitHistoryBase = 'https://github.com/openwallet-foundation/digital-wallet-and-agent-overviews-sig/commits/main/wallets/'
+
 // function to merge all the individual wallet files into a single file
 const files = readdirSync('../wallets');
 // const wallets = files.map(file => JSON.parse(readFileSync(`../wallets/${file}`)));
 const wallets = []
 for (const file of files) {
   try {
-    wallets.push(JSON.parse(readFileSync(`../wallets/${file}`)))
+    const json = JSON.parse(readFileSync(`../wallets/${file}`))
+    json.commitHistory = commitHistoryBase + file
+    wallets.push(json)
   }
   catch(e) {
     console.warn(`Error parsing ${file}: ${e}`)
