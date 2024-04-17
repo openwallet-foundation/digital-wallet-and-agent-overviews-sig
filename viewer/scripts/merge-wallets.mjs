eb659b6ceee4695206d09ba212c07018fc6daf38
Samuel Rinnetmäki <samuel@iki.fi>
2024-04-17 03:19:27 +0300

    Modify content to match new schema (#57)
    * updated content to comply with new schema
    
    Signed-off-by: Samuel Rinnetmäki <samuel@iki.fi>
    
    * added new agents
    
    Signed-off-by: Samuel Rinnetmäki <samuel@iki.fi>
    
    * addded examples
    
    Signed-off-by: Samuel Rinnetmäki <samuel@iki.fi>
    
    * slightly modified styles, added default paging of 100 items
    
    Signed-off-by: Samuel Rinnetmäki <samuel@iki.fi>
    
    * added tooltips, modified layout and styling, added commit history
    
    Signed-off-by: Samuel Rinnetmäki <samuel@iki.fi>
    
    * added commit history
    
    Signed-off-by: Samuel Rinnetmäki <samuel@iki.fi>
    
    * temp file to get Jekyll going...
    
    Signed-off-by: Samuel Rinnetmäki <samuel@iki.fi>
    
    * No longer needed
    
    Signed-off-by: Samuel Rinnetmäki <samuel@iki.fi>
    
    ---------
    
    Signed-off-by: Samuel Rinnetmäki <samuel@iki.fi>

--- viewer/scripts/merge-wallets.mjs
+++ viewer/scripts/merge-wallets.mjs
@@ -1,6 +1,20 @@
 import { readdirSync, readFileSync, writeFileSync } from 'fs';
 
+// url for the folder containing the wallet files in the GitHub repo
+const commitHistoryBase = 'https://github.com/openwallet-foundation/digital-wallet-and-agent-overviews-sig/commits/main/wallets/'
+
 // function to merge all the individual wallet files into a single file
 const files = readdirSync('../wallets');
-const wallets = files.map(file => JSON.parse(readFileSync(`../wallets/${file}`)));
+// const wallets = files.map(file => JSON.parse(readFileSync(`../wallets/${file}`)));
+const wallets = []
+for (const file of files) {
+  try {
+    const json = JSON.parse(readFileSync(`../wallets/${file}`))
+    json.commitHistory = commitHistoryBase + file
+    wallets.push(json)
+  }
+  catch(e) {
+    console.warn(`Error parsing ${file}: ${e}`)
+  }
+}
 writeFileSync('src/assets/wallets.json', JSON.stringify(wallets, null, 2));
