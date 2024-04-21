2a6626f192969483f9959f61a56720de93934e3f
Mirko Mollik <mirko.mollik@fit.fraunhofer.de>
2024-04-21 18:14:15 +0200

    fix: allow validation (#64)
    * fix: allow validation
    
    Signed-off-by: Mirko Mollik <mirko.mollik@fit.fraunhofer.de>
    
    * fix: improve error format output
    
    Signed-off-by: Mirko Mollik <mirko.mollik@fit.fraunhofer.de>
    
    ---------
    
    Signed-off-by: Mirko Mollik <mirko.mollik@fit.fraunhofer.de>

--- viewer/scripts/validate.mjs
+++ viewer/scripts/validate.mjs
@@ -7,14 +7,14 @@ const ajv = new Ajv({allowUnionTypes: true});
 addFormats(ajv);
 const profileSIGSchema = await axios.get('https://openwallet-foundation.github.io/credential-format-comparison-sig/assets/schemas/fields.json').then(res => res.data);
 ajv.addSchema(profileSIGSchema, "https://openwallet-foundation.github.io/credential-format-comparison-sig/assets/schemas/fields.json");
-const validate = ajv.compile(JSON.parse(readFileSync('../schema.json')));
+const validate = ajv.compile(JSON.parse(readFileSync('src/assets/schema.json')));
 const files = readdirSync('../wallets');
 let success = true;
 files.map(file => {
   const wallet = JSON.parse(readFileSync(`../wallets/${file}`))
   if(!validate(wallet)) {
     console.error(`Error validating ${file}:`);
-    console.error(validate.errors);
+    console.error(JSON.stringify(validate.errors, null, 2));
     success = false;
   }
 });
