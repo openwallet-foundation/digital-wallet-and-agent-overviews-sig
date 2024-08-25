1de8131d5ad280e74f36285fd0b3f4d7a73df77e
Mirko Mollik <mirkomollik@gmail.com>
2024-08-25 14:30:59 +0200

    use hash based routing
    Signed-off-by: Mirko Mollik <mirkomollik@gmail.com>

--- viewer/src/app/app.config.ts
+++ viewer/src/app/app.config.ts
@@ -4,11 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient } from '@angular/common/http';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(),
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
   ],
 };
